6af241cb5f85a7566c6fac4530c8401a2199ab33
Robert Hoffmann <rob.hoffmann@posteo.de>
2023-05-16 20:32:28 +0200

    removed moved files

--- atf_message_library/atf_message_processor/models/bundle_focus_content.py
+++
@@ -1,8 +0,0 @@
-from fhir.resources.bundle import BundleEntry
-
-
-class BundleFocusContent:
-    def __init__(self, system: str, code: str, bundle_entries: list[BundleEntry]):
-        self.system = system
-        self.code = code
-        self.bundle_entries = bundle_entries
